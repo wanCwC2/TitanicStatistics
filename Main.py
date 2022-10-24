dc29e661d3201509dc3a13560f2d3255fcd2c87f
wanCwC2 <WanPcwc0630@gmail.com>
2022-10-24 16:06:53 +0800

    Calculate the number of men and women

--- Main.py
+++ Main.py
@@ -6,3 +6,13 @@ import numpy as np
 matplotlib.style.use('ggplot')
 df = pd.read_csv('http://bit.ly/kaggletrain')
 
+male = 0 #total number of men
+female = 0 #total number of women
+
+#Male/Female total separately
+for i in range(0, df.shape[0]):
+    if df.loc[i, "Sex"]  == "male":
+        male += 1
+    if df.loc[i, "Sex"] == "female":
+        female += 1
+print(male, female)
