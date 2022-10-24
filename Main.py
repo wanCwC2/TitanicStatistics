0070abd0b41d9e7ca56ea427da8928ccfb8c816b
wanCwC2 <WanPcwc0630@gmail.com>
2022-10-24 15:04:02 +0800

    Start

---
+++ Main.py
@@ -0,0 +1,8 @@
+#Example
+import matplotlib.pyplot as plt
+import matplotlib.style
+import pandas as pd
+import numpy as np
+matplotlib.style.use('ggplot')
+df = pd.read_csv('http://bit.ly/kaggletrain')
+
