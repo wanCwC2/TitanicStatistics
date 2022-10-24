9f2a13817265dfaaf2e10a2e88f3cb55c7001551
wanCwC2 <WanPcwc0630@gmail.com>
2022-10-24 16:35:26 +0800

    Find max and min age

--- Main.py
+++ Main.py
@@ -16,3 +16,24 @@ for i in range(0, df.shape[0]):
     if df.loc[i, "Sex"] == "female":
         female += 1
 print(male, female)
+
+
+#Find max age
+#For calculate the interval
+max = 0
+min = 100
+min_i = 0
+for i in range(0, df.shape[0]):
+    if max < df.loc[i, "Age"]:
+        max = df.loc[i, "Age"]
+    if min > df.loc[i, "Age"]:
+        min = df.loc[i, "Age"]
+        min_i = df.loc[i, "Age"]
+print(max, min)
+        
+    
+'''
+#Number of people in each age group
+for i in range(0, df.shape[0]):
+    interval = 80/20
+'''
