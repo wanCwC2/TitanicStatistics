041c78553271a27e16d0dc5b670232ffe4994be5
wanCwC2 <WanPcwc0630@gmail.com>
2022-10-25 10:15:48 +0800

    Create figure

--- Main.py
+++ Main.py
@@ -9,10 +9,13 @@ df = pd.read_csv('http://bit.ly/kaggletrain')
 male = 0 #total number of men
 female = 0 #total number of women
 age = np.zeros(20)
-interval = 80/20
+interval = 80/20 #age interval
 nan_total = 0 #How many people are unknown
 pclass = [[0, 0], [0, 0], [0, 0]] #3Pclass. Every have victims and survivors
 
+#Base figure
+fig, axes = plt.subplots(2, 2)
+plt.show()
 
 for i in range(0, df.shape[0]):
     
@@ -34,6 +37,6 @@ for i in range(0, df.shape[0]):
     #Number of survivors and victims in each Pclass
     pclass[int(df.loc[i, "Pclass"])-1][int(df.loc[i, "Survived"])] += 1
 
-print(male, female)
-print(age, nan_total)
-print(pclass)
+#print(male, female)
+#print(age, nan_total)
+#print(pclass)
