24ae5c129e0a6b93bb3f14006a8c5140faa51266
wanCwC2 <WanPcwc0630@gmail.com>
2022-10-25 10:12:04 +0800

    Clean code

--- Main.py
+++ Main.py
@@ -8,35 +8,21 @@ df = pd.read_csv('http://bit.ly/kaggletrain')
 
 male = 0 #total number of men
 female = 0 #total number of women
+age = np.zeros(20)
+interval = 80/20
+nan_total = 0 #How many people are unknown
+pclass = [[0, 0], [0, 0], [0, 0]] #3Pclass. Every have victims and survivors
 
-#Male/Female total separately
+
 for i in range(0, df.shape[0]):
+    
+    #Male/Female total separately
     if df.loc[i, "Sex"]  == "male":
         male += 1
     if df.loc[i, "Sex"] == "female":
         female += 1
-print(male, female)
-
-'''
-#Find max age
-#For calculate the interval
-max = 0
-min = 100
-min_i = 0
-for i in range(0, df.shape[0]):
-    if max < df.loc[i, "Age"]:
-        max = df.loc[i, "Age"]
-    if min > df.loc[i, "Age"]:
-        min = df.loc[i, "Age"]
-        min_i = df.loc[i, "Age"]
-print(max, min)
-'''     
-
-#Number of people in each age group
-age = np.zeros(20)
-interval = 80/20
-nan_total = 0 #How many people are unknown
-for i in range(0, df.shape[0]):
+        
+    #Number of people in each age group
     if np.isnan(df.loc[i, "Age"]):
         nan_total += 1
     else:
@@ -44,11 +30,10 @@ for i in range(0, df.shape[0]):
             age[int(df.loc[i, "Age"]/interval)-1] += 1
         else:
             age[int(df.loc[i, "Age"]/interval)] += 1
-
-print(age, nan_total)
-
-#Number of survivors and victims in each Pclass
-pclass = [[0, 0], [0, 0], [0, 0]] #3Pclass. Every have victims and survivors
-for i in range(0, df.shape[0]):
+            
+    #Number of survivors and victims in each Pclass
     pclass[int(df.loc[i, "Pclass"])-1][int(df.loc[i, "Survived"])] += 1
+
+print(male, female)
+print(age, nan_total)
 print(pclass)
