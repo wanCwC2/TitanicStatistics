7bd070eda451092b64c3e807790bdca74963c288
wanCwC2 <WanPcwc0630@gmail.com>
2022-10-25 11:57:57 +0800

    Draw survivors/victims figure and Revise pclass

--- Main.py
+++ Main.py
@@ -11,7 +11,9 @@ female = 0 #total number of women
 age = np.zeros(20)
 interval = 80/20 #age interval
 nan_total = 0 #How many people are unknown
-pclass = [[0, 0], [0, 0], [0, 0]] #3Pclass. Every have victims and survivors
+#pclass = np.zeros(6) #3Pclass. Every have victims and survivors
+#pclass = [[0, 0], [0, 0], [0, 0]]
+pclass = [[0,0,0],[0,0,0]]
 
 #Base figure
 fig, axes = plt.subplots(2,2) #2rows, 2columns
@@ -34,13 +36,22 @@ for i in range(0, df.shape[0]):
             age[int(df.loc[i, "Age"]/interval)] += 1
             
     #Number of survivors and victims in each Pclass
-    pclass[int(df.loc[i, "Pclass"])-1][int(df.loc[i, "Survived"])] += 1
+    #pclass[1+(int(df.loc[i, "Pclass"])-1)*2+int(df.loc[i, "Survived"])-1] += 1
+    #pclass[int(df.loc[i, "Pclass"])-1][int(df.loc[i, "Survived"])] += 1
+    pclass[int(df.loc[i, "Survived"])][int(df.loc[i, "Pclass"])-1] += 1
 
 #Draw Male/Female figure
 axes[0][0].bar([1, 2], [male, female], tick_label=['male', 'female'])
 
 #Draw age interval figure
 axes[0][1].bar([i for i in range(0, 80, int(interval))], age)
+
+#Draw survivors/victims figure
+x = [1, 2, 3]
+width = 0.4
+axes[1][0].bar(x ,pclass[0], width = width, tick_label=['PClass1', 'PClass2', 'PClass3'], label = 'Dead')
+x2 = [num + width for num in x]
+axes[1][0].bar(x2 ,pclass[1], width = width, label = 'Live')
 
 plt.show()
 
